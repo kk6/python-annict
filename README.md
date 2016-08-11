e6db84b4452b0d3d61d2ec4ec72f5d8b323b9b11
kk6 <hiro.ashiya@gmail.com>
2016-08-11 23:44:45 +0900

    Update OAuthHandler

--- README.md
+++ README.md
@@ -3,14 +3,14 @@ Annict API wrappeer for python
 
 ## Usage
 
-### OAuth
+### Authentication
 
 First, to get the authentication code.
 
 ```python
->>> from annict.auth import AnnictOAuthHandler
->>> handler = AnnictOAuthHandler(client_id='Your client ID', client_secret='Your client secret')
->>> url = handler.get_authorize_url(scope='read write')
+>>> from annict.auth import OAuthHandler
+>>> handler = OAuthHandler(client_id='Your client ID', client_secret='Your client secret')
+>>> url = handler.get_authorization_url(scope='read write')
 >>> print(url)
 ```
 
@@ -18,6 +18,6 @@ First, to get the authentication code.
 When you open this URL in your browser, authentication code is displayed. After you copy the authentication code, and passes it to authenticate method as an argument.
 
 ```python
->>> session = handler.authenticate(code='Authentication code')
->>> print(session.access_token)
+>>> handler.authenticate(code='Authentication code')
+>>> print(handler.get_access_token())
 ```
--- annict/auth.py
+++ annict/auth.py
@@ -5,26 +5,32 @@ from urllib.parse import urljoin
 from rauth import OAuth2Service
 
 
-class AnnictOAuthHandler(object):
-    NAME = 'annict'
-    BASE_URL = 'https://api.annict.com'
+class OAuthHandler(object):
 
-    def __init__(self, client_id, client_secret, redirect_uri='urn:ietf:wg:oauth:2.0:oob'):
+    def __init__(self, client_id, client_secret, name='annict', base_url='https://api.annict.com',
+                 redirect_uri='urn:ietf:wg:oauth:2.0:oob'):
         self.client_id = client_id
         self.client_secret = client_secret
+        self.name = name
+        self.base_url = base_url
         self.redirect_uri = redirect_uri
         self.oauth = None
+        self.auth_session = None
 
-    def get_authorize_url(self, scope='read'):
-        authorize_url = urljoin(self.BASE_URL, '/oauth/authorize')
-        access_token_url = urljoin(self.BASE_URL, '/oauth/token')
+    def get_authorization_url(self, scope='read'):
+        authorize_url = urljoin(self.base_url, '/oauth/authorize')
+        access_token_url = urljoin(self.base_url, '/oauth/token')
         self.oauth = OAuth2Service(client_id=self.client_id, client_secret=self.client_secret,
-                                   authorize_url=authorize_url, base_url=self.BASE_URL, name=self.NAME,
+                                   authorize_url=authorize_url, base_url=self.base_url, name=self.name,
                                    access_token_url=access_token_url)
         params = {'scope': scope, 'response_type': 'code', 'redirect_uri': self.redirect_uri}
         return self.oauth.get_authorize_url(**params)
 
     def authenticate(self, code, decoder=lambda s: json.loads(s.decode('utf8'))):
         data = {'code': code, 'grant_type': 'authorization_code', 'redirect_uri': self.redirect_uri}
-        return self.oauth.get_auth_session(data=data, decoder=decoder)
+        session = self.oauth.get_auth_session(data=data, decoder=decoder)
+        self.auth_session = session
 
+    def get_access_token(self):
+        if self.auth_session:
+            return self.auth_session.access_token
