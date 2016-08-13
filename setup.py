b7c41c31e90b8eebf5df6e471d69cb953d45e8e3
kk6 <hiro.ashiya@gmail.com>
2016-08-14 01:46:59 +0900

    Add setpu.py/cfg

---
+++ setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+tests_require = ['pytest',]
+
+setup(
+    name='annict',
+    version='0.0.1',
+    license="MIT",
+    author="kk6",
+    author_email="hiro.ashiya@gmail.com",
+    url="https://github.com/kk6/python-annict",
+    packages=find_packages(exclude=['tests*']),
+    install_requires=[
+        'rauth',
+        'requests',
+        'requests-cache',
+        'furl',
+        'arrow',
+    ],
+    setup_requires=['pytest-runner',],
+    tests_require=tests_require,
+    extras_require={'testing': tests_require},
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 3',
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries"
+    ],
+)
+
