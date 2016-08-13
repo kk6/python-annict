6b7097c523503a2ec33ca4bca82ccd59e11c8ed7
kk6 <hiro.ashiya@gmail.com>
2016-08-14 03:21:47 +0900

    OAuth2Serviceインスタンス生成を__init__で。

--- annict/auth.py
+++ annict/auth.py
@@ -14,15 +14,15 @@ class OAuthHandler(object):
         self.name = name
         self.base_url = base_url
         self.redirect_uri = redirect_uri
-        self.oauth = None
         self.auth_session = None
+        self.oauth = OAuth2Service(client_id=client_id,
+                                   client_secret=client_secret,
+                                   name=name,
+                                   base_url=base_url,
+                                   authorize_url=urljoin(base_url, '/oauth/authorize'),
+                                   access_token_url=urljoin(base_url, '/oauth/token'))
 
     def get_authorization_url(self, scope='read'):
-        authorize_url = urljoin(self.base_url, '/oauth/authorize')
-        access_token_url = urljoin(self.base_url, '/oauth/token')
-        self.oauth = OAuth2Service(client_id=self.client_id, client_secret=self.client_secret,
-                                   authorize_url=authorize_url, base_url=self.base_url, name=self.name,
-                                   access_token_url=access_token_url)
         params = {'scope': scope, 'response_type': 'code', 'redirect_uri': self.redirect_uri}
         return self.oauth.get_authorize_url(**params)
 
