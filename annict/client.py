647d587e4be51348546df8a46e87e206a3819f5c
kk6 <hiro.ashiya@gmail.com>
2016-08-14 23:40:18 +0900

    requests.request使えばよかった

--- annict/client.py
+++ annict/client.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-from operator import methodcaller
-
 import requests
 from furl import furl
 
@@ -12,19 +10,19 @@ class Client(object):
         self.base_url = base_url
         self.api_version = api_version
 
-    def _request(self, http_method, path, params=None):
+    def _request(self, method, path, params=None):
         params['access_token'] = self.access_token
 
         kwargs = {}
-        if http_method == 'post' or http_method == 'patch':
+        if method == 'post' or method == 'patch':
             kwargs['data'] = params
-        elif http_method == 'get':
+        elif method == 'get':
             kwargs['params'] = params
 
         url = furl(self.base_url)
         url.path.add(self.api_version).add(path)
-        m = methodcaller(http_method, url.url, **kwargs)
-        resp = m(requests)
+        resp = requests.request(method, url.url, **kwargs)
+
         if resp.status_code == 200:
             return resp.json()
         elif resp.status_code == 204:
