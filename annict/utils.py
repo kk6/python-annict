88560f217a901f9c3ce3e747ade09c94047515e4
kk6 <hiro.ashiya@gmail.com>
2017-02-08 16:38:38 +0900

    Add stringify_boolean

--- annict/utils.py
+++ annict/utils.py
@@ -17,3 +17,8 @@ def do_not_stringify(arg):
 @stringify.register(set)
 def stringify_list(arg):
     return ','.join([str(o) for o in arg])
+
+
+@stringify.register(bool)
+def stringify_boolean(arg):
+    return str(arg).lower()
