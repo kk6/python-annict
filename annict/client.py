79f116c1a2acc1ae975fa1d1a4715c612357272a
kk6 <hiro.ashiya@gmail.com>
2017-02-22 11:42:47 +0900

    4,500系エラー対応

--- annict/client.py
+++ annict/client.py
@@ -3,6 +3,10 @@ import requests
 from furl import furl
 
 
+class AnnictError(Exception):
+    pass
+
+
 class Client(object):
 
     def __init__(self, access_token, base_url='https://api.annict.com', api_version='v1'):
@@ -27,6 +31,9 @@ class Client(object):
             return resp.json()
         elif resp.status_code == 204:
             return True
+        elif (resp.status_code // 100) in (4, 5):
+            e = resp.json()['errors'][0]
+            raise AnnictError(f"{e['type']}:{e['message']}: {e['developer_message']}")
         else:
             return resp
 
