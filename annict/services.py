ee7137464619d9fb18e10fe22b7e8f7fbdd0957f
kk6 <hiro.ashiya@gmail.com>
2017-04-11 23:25:15 +0900

    Stopped the variable length argument of the API and made it explicit.

--- annict/services.py
+++ annict/services.py
@@ -2,58 +2,35 @@
 from furl import furl
 import requests
 
-from .exceptions import AnnictError
 from .utils import stringify
 
 
 class APIMethod(object):
 
-    def __init__(self, api, path, method, required_params, optional_params, target_id=None,
-                 payload_type=None, payload_list=False):
+    def __init__(self, api, path, method, allowed_params=None, payload_type=None, payload_list=False):
         self.api = api
         self.path = path
         self.method = method
-        self.allowed_params = required_params + optional_params
-        self.target_id = target_id
+        self.allowed_params = allowed_params
         self.payload_type = payload_type
         self.payload_list = payload_list
 
-    def build_parameters(self, args, kwargs):
-        params = {}
-        for i, arg in enumerate(args):
-            if arg is None:
-                continue
-            try:
-                params[self.allowed_params[i]] = stringify(arg)
-            except IndexError:
-                raise AnnictError("Too many parameters supplied.")
-
-        for k, arg in kwargs.items():
-            if arg is None:
-                continue
-            if k in params:
-                raise AnnictError(f"Multiple values for parameter '{k}' supplied.")
-            if k not in self.allowed_params:
-                raise AnnictError(f"Unknown keyword supplied: '{k}'")
-
-            params[k] = stringify(arg)
-
-        return params
-
-    def build_path(self):
-        if self.target_id:
-            self.path = '/'.join([self.path, str(self.target_id)])
+    def build_path(self, id_=None):
+        if id_ is not None:
+            self.path = '/'.join([self.path, str(id_)])
 
     def build_url(self):
-        self.build_path()
         url = furl(self.api.base_url)
         url.path.add(self.api.api_version).add(self.path)
         return url.url
 
-    def __call__(self, *args, **kwargs):
-        url = self.build_url()
-        params = self.build_parameters(args, kwargs)
+    def build_parameters(self, dic):
+        params = {key: stringify(dic[key]) for key in self.allowed_params if dic[key]}
         params['access_token'] = self.api.token
+        return params
+
+    def __call__(self, params):
+        url = self.build_url()
         resp = requests.request(self.method, url, params=params)
 
         resp.raise_for_status()
